39d914d3074061009a3121d7763c11b377af6d80
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-08-15 21:10:10 +0300

    Initial commit

---
+++ Core/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.LoadFile(File.OpenRead(@"..\..\..\..\example.rtf"), RichTextBoxStreamType.RichText);
+        }
+
+        private void RichTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            MessageBox.Show(this, e.LinkText, "link clicked");
+        }
+    }
+}
